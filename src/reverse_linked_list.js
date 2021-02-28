76c354c7b6fb79b46d8a000c775ffa997c7a599d
Anastasia Pirvu <anastasia.pirvu@gmail.com>
2021-02-28 11:57:28 +0100

    Reverse linked list

--- src/reverse_linked_list.js
+++ src/reverse_linked_list.js
@@ -1,6 +1,6 @@
 /*------------------
 Finish the "reverseList" method.
-This method should reverse the linkedList
+This method should revers e the linkedList
 so that the head and tail switch, and all
 pointers move into the opposite direction.
 
@@ -54,18 +54,37 @@ class LinkedList {
     // So the current head will be pointing "the other way" (which means null), and therefore will become the new tail of the list
     // Then we will take the following node on the list, and reverse its pointer, so it points to what has previously the Head (but now is the tail)
     // And we will do this progressively with each node, till we reach the "current tail", that will become the new head
+
     // Your code starts here:
-    // Create a variable 'node' to store the reference to this.head. This var will keep track of the "current" node as we advance on the list
+    // Create a variable 'node' to store the reference to this.head.
+    //This var will keep track of the "current" node as we advance on the list
     // Create another var 'initialHead', to also point to this.head. We will need it at the end.
-    // Create a variable to store the "previous" node. Since we start at the head, "previous" should be null when initialized (because there's nothing "before" the head)
+
+    let node = this.head;
+    let initialHead = this.head;
+    let prev = null;
+
+    // Create a variable to store the "previous" node. Since we start at the head,
+    //"previous" should be null when initialized (because there's nothing "before" the head)
     // while there is a node to visit (so, node exists)
     // create a var to save the location of the next node, or you will lose it!!!
     // reverse the current node's "next" pointer, it should now point to the "previous" node
     // update previous node to be the current node
     // update current node to the next node (you saved it) or null at end of list
     // end of while
+
+    while (head !== null) {
+      this.head = prev;
+      let newVar = node.next;
+      node.next = prev;
+      prev = node;
+      node = node.next;
+    }
+
     // make the head point to what was, until now, the tail
     // make the tail point to what was initially the head
+    this.head.next = this.tail;
+    this.tail = initialHead;
   }
 }
 
